4c946d6018f122e8ec593e3db3212d8a064931a1
liyangdan <liyangdan@cai-inc.com>
2018-12-17 12:42:23 +0800

    test

--- src/main/java/com/example/demo/validator/HibernateValidatorConfiguration.java
+++ src/main/java/com/example/demo/validator/HibernateValidatorConfiguration.java
@@ -49,7 +49,7 @@ public class HibernateValidatorConfiguration {
         return Validation
                 .byProvider(HibernateValidator.class)
                 .configure()
-                //自定义message名称，否则只会读取默认配置下的ValidationMessage这个文件
+                //自定义message名称，否则只会读取默认配置下的ValidationMessage这个文件                .messageInterpolator(new ResourceBundleMessageInterpolator( new AggregateResourceBundleLocator(Arrays.asList("MyMessages1","MyMessages2","MyMessages3"))))
                 .messageInterpolator(new ResourceBundleMessageInterpolator(new PlatformResourceBundleLocator("myValidationMessages" )))
                 //快速返回模式，有一个验证失败立即返回错误信息
                 .failFast(true)
